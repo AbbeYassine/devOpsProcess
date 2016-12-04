005480594f404c3364895ab6fd082ae3820aaaed
Samet Oussema <sametoussema@gmail.com>
2016-12-04 19:47:21 +0100

    adding main content App, integring Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Parent image
+FROM maven:3-jdk-8
+
+# Update the VM
+RUN apt-get update
+
+#Create app directory
+RUN mkdir -p /usr/src/my-app
+WORKDIR /usr/src/my-app
+
+# Bundle app source
+COPY . /usr/src/my-app
+
+# Run maven install mvn plugins and to build the project
+RUN mvn install
+
+#START THE APP
+ENTRYPOINT ["java","-cp","target/my-app-1.0.jar","com.mycompany.app.App"]
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,7 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
 public final class App {
 
 
@@ -10,6 +9,29 @@ public final class App {
     }
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("My-App is running !");
+        int x, y, res = 0;
+        System.out.println("Choisissez un operateur :");
+        System.out.println("1) +");
+        System.out.println("2) *");
+        int operator = scanner.nextInt();
+
+        System.out.println("Entrez 2 entiers :");
+        scanner.nextLine();
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        Calcul calcul = new Calcul();
+        switch (operator) {
+            case 1:
+                res = calcul.additionner(x, y);
+                break;
+            case 2:
+                res = calcul.multiplier(x, y);
+                break;
+        }
+
+
+        System.out.println("resultat : " + res);
     }
 }
